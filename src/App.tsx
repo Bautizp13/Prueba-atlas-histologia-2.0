1be89f23b1f37e12540a945b8ca16bbd1cbd53cb
Bautizp13 <zarate.juan@uncuyo.edu.ar>
2026-08-19 22:11:25 -0300

    Import ImageMarkerTool component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import ImageMarkerTool from './components/ImageMarkerTool';
 import { MastocitoPensando, MastocitoCorriendo, MastocitoSaludando, MastocitoSorprendido } from './Mastocito'
 
 // ─── Imágenes ────────────────────────────────────────────────────────────────
